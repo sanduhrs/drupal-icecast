34331660fbe23f67b247ca39db5bcfe84f4e026a
mark burdett <mfburdett@gmail.com>
2008-07-04 04:57:31 +0000

    add template for graph page

---
+++ yplog/yplog-graph.tpl.php
@@ -0,0 +1,11 @@
+<?php
+// $Id$
+?>
+
+<div class="yplog-graph">
+  <?php print $image; ?>
+</div>
+
+<strong>
+  <?php print t('Listen: !listen_link', array('!listen_link' => $listen_link)); ?>
+</strong>
